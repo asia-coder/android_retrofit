398e6ee0085409356af3d8427718677e6e8529c0
Muydinov Dilshod <uktamovich95@gmail.com>
2018-10-19 19:25:19 +0500

    RecycleView and Retrofit

---
+++ app/src/main/java/network/GetEmployeeDataService.java
@@ -0,0 +1,11 @@
+package network;
+
+import model.EmployeeList;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface GetEmployeeDataService {
+    @GET("retrofit-demo.php")
+    Call<EmployeeList> getEmployeeData(@Query("company_no") int companyNo);
+}
